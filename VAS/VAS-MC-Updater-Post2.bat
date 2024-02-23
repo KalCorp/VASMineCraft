4537ee05638c47dae1e0e3621ef9c25a3979d0da
KalCorp <12770250+KalCorp@users.noreply.github.com>
2024-02-23 13:41:50 -0500

    Update VAS-MC-Updater-Post2.bat

--- VAS/VAS-MC-Updater-Post2.bat
+++ VAS/VAS-MC-Updater-Post2.bat
@@ -1,5 +1,5 @@
 
-Del /Q /F "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Pre.bat"
-REN "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Pre2.bat" VAS-MC-Updater-Pre.bat
+Del /Q /F "%localFolder%\VAS\VAS-MC-Updater-Pre.bat"
+REN "%localFolder%\VAS\VAS-MC-Updater-Pre2.bat" VAS-MC-Updater-Pre.bat
 
 Echo Done!
