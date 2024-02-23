61890908dfcafd5027c2170203773f15f77e824a
KalCorp <12770250+KalCorp@users.noreply.github.com>
2024-02-23 13:05:16 -0500

    Update VAS-MC-Updater-Pre2.bat

--- VAS/VAS-MC-Updater-Pre2.bat
+++ VAS/VAS-MC-Updater-Pre2.bat
@@ -32,8 +32,8 @@ powershell -command "& {Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Comp
 RoboCopy "%localFolder%\%repoName%-%branch%" "%INST_MC_DIR%" *.* /S /XD mods >> %LogPath%
 RoboCopy "%localFolder%\%repoName%-%branch%\mods" "%INST_MC_DIR%\mods" *.* /MIR /S >> %LogPath%
 
-Del /Q "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Post.bat"
-REN "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Post2.bat" "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Post.bat"
+if exist "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Post.bat" Del /Q "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Post.bat"
+if exist "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Post2.bat" REN "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Post2.bat" "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Post.bat"
 
 
 Echo DONE
