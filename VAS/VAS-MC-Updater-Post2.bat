9dcaade8092021a1b4e8a6c64907a0f1d45a05fa
KalCorp <12770250+KalCorp@users.noreply.github.com>
2024-02-23 13:02:09 -0500

    Add files via upload

---
+++ VAS/VAS-MC-Updater-Post2.bat
@@ -0,0 +1,4 @@
+
+Del /Q "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Pre.bat"
+REN "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Pre2.bat" "%localFolder%\%repoName%-%branch%\VAS\VAS-MC-Updater-Pre.bat"
+
